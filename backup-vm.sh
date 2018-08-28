dbd468107b6b30aed2c9fde74a9d214c4d1f5191
Legolabs srl <34338274+legolabs@users.noreply.github.com>
2018-08-28 13:36:15 +0200

    Create backup-vm.sh

---
+++ backup-vm.sh
@@ -0,0 +1,102 @@
+#!/bin/bash
+
+printf "\n"
+
+
+# Check arguments
+if [ "$#" -ne 2 ]; then
+  printf "Invalid number of arguments!\n\n"
+  printf "Usage: $0 <vm_name> <destination_path>\n\n"
+  exit 1
+fi
+
+VM_NAME=$1
+DEST_PATH=$2
+
+
+# Check if VM exists
+printf "Looking for VM \"$VM_NAME\"... "
+CHECKVM=`VBoxManage list vms | grep '"'$VM_NAME'"' | head -n 1`
+
+if [ -z "$CHECKVM" ]
+then
+  printf "Not found! -> Quitting!\n\n"
+  exit 2
+else
+  printf "Found\n"
+fi
+
+
+# Get VM folder
+printf "Searching VM folder for %s... " "$VM_NAME"
+#VM_FOLDER=`VBoxManage showvminfo "$VM_NAME" | grep "Log folder:" | sed 's/.*Log folder:\(.*\).*/\1/;t;d' | head -n 1 | sed -e 's/^[ \t]*//'`"/VBox.log"
+VM_FOLDER=`VBoxManage list systemproperties | grep "Default machine folder:" | sed 's/.*Default machine folder:\(.*\).*/\1/;t;d' | head -n 1 | sed -e 's/^[ \t]*//'`/"$VM_NAME"
+
+if [ -z "$VM_FOLDER" ]
+then
+  printf "Not found! Quitting!\n\n"
+  exit 3
+else
+  printf "Found $VM_FOLDER\n"
+fi
+
+
+printf "Checking if VM folder exists... "
+# Check if source folder exists
+if [ ! -d "$VM_FOLDER" ]
+then
+  printf "Non found! Quitting!\n\n"
+  exit 4
+else
+  printf "Found!\n"
+fi
+
+
+# Check if VM is running
+printf "Checking if VM is running... "
+VM_RUNNING=`VBoxManage list runningvms | grep '"'$VM_NAME'"' | head -n 1`
+
+if [ -z "$VM_RUNNING" ]
+then
+  printf "Not running.\n"
+else
+  printf "Running.\n"
+  printf "Try stopping VM... "
+  vboxmanage controlvm "$VM_NAME" savestate
+
+  if [ $? -eq 0 ]
+  then
+    printf "Done\n"
+  else
+    printf "Failed! Check VM state ASAP!\n\n"
+    exit 5
+  fi
+fi
+
+
+# Sends file without compression and replace modified bytes without using temp file
+# --delete is needed in order to remove old .sav file
+printf "Starting sync with destination... "
+
+rsync -a --inplace --delete "$VM_FOLDER" "$DEST_PATH"
+
+if [ $? -eq 0 ]
+then
+  printf "Done\n"
+else
+  printf "Failed! Check backup consistency ASAP!\n\n"
+  exit 6
+fi
+
+
+# Start VM
+printf "Starting VM... "
+vboxmanage startvm "$VM_NAME" --type headless
+
+if [ $? -eq 0 ]
+then
+  printf "Done\n\n"
+else
+  printf "Failed! Check VM state ASAP!\n\n"
+  exit 7
+fi
